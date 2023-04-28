4f2a1adf43e5d81d489ce2beeda0a3033f8e9b03
sb412 <91774615+sb412@users.noreply.github.com>
2023-04-28 20:49:53 +0530

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -3,6 +3,41 @@
 #define repi(i,a,b) for(int i=a;i<b;i++)
 using namespace std;
  
+//disjoint_set  name(input_size)
+	
+// class disjoint_set {
+//     vector<int> v;
+//     int sz;
+// public:
+//     disjoint_set(int n) {
+//         makeset(n);
+//     }
+
+//     void makeset(int n) {
+//         v.resize(n);
+//         iota(v.begin(), v.end(), 0);
+//         sz = n;
+//     }
+
+//     int find(int i) {
+//         if (i != v[i])
+//             v[i] = find(v[i]);
+//         return v[i];
+//     }
+    
+//     void join(int i, int j) {
+//         int ri = find(i), rj = find(j);
+//         if (ri != rj) {
+//             v[ri] = rj;
+//             sz--;
+//         }
+//     }
+    
+//     int size() {
+//         return sz;
+//     }
+// };
+
 int parent[100001];
 
 void make_set(int v) {
